69f3001668844d9ad5361455a86af523fd355889
bamb1z <michelle.morilhas@gmail.com>
2021-08-10 20:49:35 -0300

    estilizando o jogo e adicionando botão refresh

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#0f0a1e";
     context.fillRect( 0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for (i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "#00ff41" ;
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function criarComida(){
+    context.fillStyle = "orange";
+    context.fillRect(comida.x, comida.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -35,8 +44,16 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert(` Game Over :( `);
+        }
+    }
+
     criarBG();
     criarCobrinha();
+    criarComida();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -46,7 +63,14 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != comida.x || snakeY != comida.y) {
+        snake.pop();
+    }
+    else{
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
@@ -54,8 +78,13 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead);
+
 }
 
 let jogo = setInterval(iniciarJogo, 100);
+
+function refreshPage(){
+    window.location.reload();
+}
 
 
